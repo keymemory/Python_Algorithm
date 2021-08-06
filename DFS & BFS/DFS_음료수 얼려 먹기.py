34e2f03927d32531a729a6ace4b92a08f3f25a81
keymemory@naver.com <keymemory@naver.com>
2021-08-06 17:20:48 +0900

    Initial commit

--- DFS & BFS/DFS_음료수 얼려 먹기.py
+++ DFS & BFS/DFS_음료수 얼려 먹기.py
@@ -9,9 +9,10 @@ for i in range(n):
 
 # DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
 def dfs(x, y):
-    # 현재 노드를 아직 방문하지 않았다면
+    # 주어진 범위를 벗어나는 경우에는 즉시 종료
     if x <= -1 or x >= n or y <= -1 or y >= m:
         return False
+    # 현재 노드를 아직 방문하지 않았다면
     if graph[x][y] == 0:
         # 해당 노드 방문 처리
         graph[x][y] = 1
@@ -21,9 +22,6 @@ def dfs(x, y):
         dfs(x + 1, y)
         dfs(x, y + 1)
         return True
-    return False
-
-    # 주어진 범위를 벗어나는 경우에는 즉시 종료
     return False
 
 
